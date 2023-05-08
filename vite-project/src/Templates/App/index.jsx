3b91ad09e6aea94fc297f521377c63a1ef1dac7a
3vinicius <amorimvinicius3@gmail.com>
2023-05-08 16:47:18 -0300

    configuration style components

---
+++ vite-project/src/Templates/App/index.jsx
@@ -0,0 +1,15 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+    <>
+      <div>
+        <Styled.Wrapper>
+          <h1>olá</h1>
+        </Styled.Wrapper>
+      </div>
+    </>
+  );
+}
+
+export default Home;
--- vite-project/src/index.jsx
+++ vite-project/src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Home from './Templates/App'
+import { GlobalStyles } from './Styles/global-styles'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <Home />
+    <GlobalStyles/>
+  </React.StrictMode>
 )
