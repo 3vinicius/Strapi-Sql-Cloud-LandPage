4240577aa90dc05103be39f5b3a21ad9d1b4bdeb
Vinícius Amorim Alves Omena <63747127+3vinicius@users.noreply.github.com>
2023-05-11 21:04:02 -0300

    delete vite-project

--- vite-project/src/Components/Heading/stories.jsx
+++
@@ -1,8 +0,0 @@
-import { Heading } from '.';
-
-export default {
-  title: 'Heading',
-  components: Heading,
-};
-
-export const Template = (args) => <Heading {...args} />;
--- vite-project/src/Components/Heading/styles.js
+++
@@ -1,7 +0,0 @@
-import styled, {css} from 'styled-components';
-
-export const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
