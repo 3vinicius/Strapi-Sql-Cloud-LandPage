96431c8ff44ddd431514ac32386b080cbeeebafb
Vinícius Amorim Alves Omena <63747127+3vinicius@users.noreply.github.com>
2023-05-11 21:03:19 -0300

    delete vites dire

--- vites/src/Components/Heading/index.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export const Heading = ({ children }) => {
-  return (
-    <div>
-      <div>{children}</div>
-      <input type="button" value="Input" />
-    </div>
-  );
-};
