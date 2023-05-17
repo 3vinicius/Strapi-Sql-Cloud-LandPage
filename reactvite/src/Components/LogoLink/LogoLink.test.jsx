091dd905ea558bcdabd60acbc2ffef632699ee99
3vinicius <amorimvinicius3@gmail.com>
2023-05-17 15:50:17 -0300

    finish componet LogoLink

--- reactvite/src/Components/LogoLink/LogoLink.test.jsx
+++ reactvite/src/Components/LogoLink/LogoLink.test.jsx
@@ -10,13 +10,68 @@ const mock = {
 
 describe('<LogoLink />', () => {
   it('Should render <LogoLink />', () => {
-    const { debug } = renderTheme(<LogoLink {...mock} />);
-    debug();
+    renderTheme(<LogoLink {...mock} />);
   });
 
   it('Should render correct props mock', () => {
     const { container } = renderTheme(<LogoLink {...mock} />);
-    const child = container.querySelector('h1');
-    expect(child).toMatchInlineSnapshot();
+    const child = container.querySelector('a');
+
+    expect(child).toHaveAttribute('href', mock.link);
+    expect(screen.getByRole('img')).toHaveAttribute('alt', mock.text);
+    expect(screen.getByRole('img')).toHaveAttribute('src', mock.scrImg);
+  });
+
+  it('Should render correct props mock from text', () => {
+    const { container } = renderTheme(
+      <LogoLink text={mock.text} link={mock.link} />,
+    );
+    const child = container.querySelector('a');
+
+    expect(child).toHaveAttribute('href', mock.link);
+    expect(screen.getByRole('link')).toBeInTheDocument();
+
+    expect(screen.queryByAltText(mock.text)).not.toBeInTheDocument();
+    expect(screen.queryByText(mock.text)).toBeInTheDocument();
+  });
+
+  it('Should render correct styles from text', () => {
+    const { container } = renderTheme(
+      <LogoLink text={mock.text} link={mock.link} />,
+    );
+    const child = container.querySelector('a');
+
+    expect(child).toHaveStyle({
+      display: 'flex',
+      'align-items': 'center',
+    });
+  });
+
+  it('Should render correct styles from img', () => {
+    const { container } = renderTheme(<LogoLink {...mock} />);
+    const child = container.querySelector('img');
+
+    expect(child).toHaveStyle({
+      'max-width': '8rem',
+      'max-height': '8rem',
+    });
+  });
+
+  it('Should render correct styles from heading', () => {
+    renderTheme(<LogoLink text={mock.text} link={mock.link} />);
+    const child = screen.queryByRole('heading');
+
+    expect(child).toHaveStyle({
+      'text-transform': 'uppercase',
+      'font-size': '2.4rem',
+    });
   });
 });
+
+// `}
+//   display: flex;
+//   align-items: center;
+//   > img {
+//     max-width: 8rem;
+//     max-height: 8rem;
+//   }
