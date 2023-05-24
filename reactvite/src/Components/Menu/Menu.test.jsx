dff5e7153a402149005fdaf6b3fdba04c5cc4e7a
3vinicius <amorimvinicius3@gmail.com>
2023-05-24 17:14:35 -0300

    create test in component <Menu />

--- reactvite/src/Components/Menu/Menu.test.jsx
+++ reactvite/src/Components/Menu/Menu.test.jsx
@@ -1,5 +1,5 @@
 import { Menu } from '.';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import mocklinks from '../NavBar/mock.links';
 
@@ -17,22 +17,46 @@ describe('<Menu />', () => {
   it('Should render correct display mobile', () => {
     renderTheme(
       <Menu
-        styles={{ with: '750px' }}
+        styles={{ width: '750px' }}
         links={mocklinks}
         logoData={mockLogoData}
       />,
     );
-    screen().g
+    const child = screen.getByLabelText('Close/Open Menu');
+
+    expect(child).toHaveStyleRule('display', 'none');
+    expect(child).toHaveStyleRule('display', 'flex', {
+      media: '(max-width:768px)',
+    });
   });
 
   it('Should render correct event <Menu />', () => {
     renderTheme(<Menu links={mocklinks} logoData={mockLogoData} />);
+
+    const child = screen.getByLabelText('Close/Open Menu');
+
+    expect(child.nextSibling).toHaveStyleRule('visibility', 'hidden', {
+      media: '(max-width:768px)',
+    });
+    expect(screen.getByLabelText('Open Menu')).toBeInTheDocument();
+    fireEvent.click(child);
+    expect(screen.getByLabelText('Close Menu')).toBeInTheDocument();
+    expect(child.nextSibling).toHaveStyleRule('visibility', 'visible', {
+      media: '(max-width:768px)',
+    });
+  });
+
+  it('Should not render links <Menu />', () => {
+    renderTheme(<Menu logoData={mockLogoData} />);
+    const nav = screen.getByLabelText('Main menu');
+    expect(nav.firstChild).not.toBeInTheDocument();
   });
 
   it('Should render correct snapshot', () => {
     const { container } = renderTheme(
       <Menu links={mocklinks} logoData={mockLogoData} />,
     );
-    screen.debug(container);
+
+    expect(container).toMatchSnapshot();
   });
 });
