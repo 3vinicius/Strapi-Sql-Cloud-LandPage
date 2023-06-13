431a7bc1ae42b4ee2de251967df2390babb9b3f4
3vinicius <amorimvinicius3@gmail.com>
2023-06-13 15:30:53 -0300

    feat: create async function with fetch and template PageNotFaund

---
+++ reactvite/src/templates/PageNotFound/index.jsx
@@ -0,0 +1,28 @@
+import p from 'prop-types';
+import { GridContent } from '../../Components/GridContent';
+
+import styled, { css } from 'styled-components';
+
+const Container = styled.div`
+  ${() => css`
+    a{
+      color: #DC143C
+    }
+  `}
+`;
+
+export const PageNotFound = () => {
+  return (
+    <Container>
+      <GridContent
+        background={true}
+        title="Error 404 !"
+        html="<p>A página não foi encontrada <a href='/'>Clique para voltar</a> </p>"
+      />
+    </Container>
+  );
+};
+
+PageNotFound.propTypes = {
+  children: p.node.isRequired,
+};
