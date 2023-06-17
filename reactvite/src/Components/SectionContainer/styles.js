b3d8d7c60a80e734fa8cf36194f472af8ff4458c
3vinicius <amorimvinicius3@gmail.com>
2023-06-17 12:07:57 -0300

    feat: error correction response styles SectionContainer

--- reactvite/src/Components/SectionContainer/styles.js
+++ reactvite/src/Components/SectionContainer/styles.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 export const Container = styled.div`
   ${({ theme }) => css`
     max-width: 120rem;
-    margin: 0 auto;
+    margin:auto;
     padding: ${theme.spacings.large};
   `}
 `;
